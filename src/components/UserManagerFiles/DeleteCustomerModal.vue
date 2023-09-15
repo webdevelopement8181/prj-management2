f9f7fdf0c79ed37a1b8b99b8454a6d8f5c1455e4
ghazale fattahi <ghazale8181fattahi@gmail.com>
2023-09-15 15:15:10 +0330

    latest update

--- src/components/UserManagerFiles/DeleteCustomerModal.vue
+++ src/components/UserManagerFiles/DeleteCustomerModal.vue
@@ -1,80 +1,79 @@
 <template>
-    <div>
-      <b-row class="mt-2 mb-3">
-        <h6 class="text-secondary">
-          Are you sure you want to delete this user ?
-        </h6>
-      </b-row>
-      <b-row class="mt-2 mb-3">
-        <p class="text-danger">
-          This action is not reversible and may result in the loss if important
-          data.
-        </p>
-      </b-row>
-      <b-row class="mt-4">
-        <b-col>
-          <b-button variant="danger" @click="removeCustomerFromData"
-            >Delete Customer</b-button
-          >
-        </b-col>
-        <b-col>
-          <b-button variant="warning" @click="triggerClose">Close</b-button>
-        </b-col>
-      </b-row>
-    </div>
-  </template>
+  <div>
+    <b-row class="mt-2 mb-3">
+      <h6 class="text-secondary">
+        Are you sure you want to delete this user ?
+      </h6>
+    </b-row>
+    <b-row class="mt-2 mb-3">
+      <p class="text-danger">
+        This action is not reversible and may result in the loss if important
+        data.
+      </p>
+    </b-row>
+    <b-row class="mt-4">
+      <b-col>
+        <b-button variant="danger" @click="removeCustomerFromData">Delete Customer</b-button>
+      </b-col>
+      <b-col>
+        <b-button variant="warning" @click="triggerClose">Close</b-button>
+      </b-col>
+    </b-row>
+  </div>
+</template>
   
-  <script>
-  // ,mapActions
- import { mapGetters } from "vuex";
-  
-  export default {
-    name: "DeleteCustomerModal",
-    props: {
-      customerId: Number,
-    },
-    computed:{
-      ...mapGetters("customer", ["getCustomerById"]),
-      // ...mapActions("customer", ["deleteCustomer"]),
-    },
-    created() {
+<script>
+// ,mapActions
+import { mapGetters } from "vuex";
+
+export default {
+  name: "DeleteCustomerModal",
+  props: {
+    customerId: Number,
+  },
+  computed: {
+    ...mapGetters("customer", ["getCustomerById"]),
+    // ...mapActions("customer", ["deleteCustomer"]),
+  },
+  created() {
     // Fetch the customer data from Vuex using the customer ID
     this.customer = this.getCustomerById(this.customerId);
 
-    },
+  },
   //   // this.username = this.getUsername;
   // },
   //   watch: {
-   
-    customerId: {
-      immediate: true,
-      handler(newCustomerId) {
-        this.customer = this.getCustomerById(newCustomerId);
-        // console.log(this.customer)
-      },
-    
+
+  customerId: {
+    immediate: true,
+    handler(newCustomerId) {
+      this.customer = this.getCustomerById(newCustomerId);
+      console.log("newCustomerId: ", newCustomerId)
+      // console.log(this.customer)
     },
+
+  },
   // },
 
-    methods: {
-    
-      triggerClose() {
-        this.$emit("closeDeleteModal");
-      },
-     
-      removeCustomerFromData() {
+  methods: {
 
-        this.$store.dispatch('customer/deleteCustomer', this.customer.id) .then(() => {
-        console.log("this is my function")
-        this.$emit("reloadDataTable");
-            this.$emit("showDeleteAlert");
-            this.$emit("closeDeleteModal");
-    })
-    .catch((error) => {
-      console.error("Error deleting customer:", error);
-    });
-},
-
+    triggerClose() {
+      this.$emit("closeDeleteModal");
     },
-  };
-  </script>
+
+    removeCustomerFromData() {
+
+      this.$store.dispatch('customer/deleteCustomer', this.customer.customerId).then(() => {
+        console.log(this.customer.customerId)
+        this.$emit("reloadDataTable");
+        this.$emit("showDeleteAlert");
+        this.$emit("closeDeleteModal");
+      })
+        .catch((error) => {
+          console.error("Error deleting customer:", error);
+        }); 
+    },
+
+  },
+};
+</script>
--- src/store/modules/Customer/customer.js
+++ src/store/modules/Customer/customer.js
@@ -9,109 +9,112 @@ const mutations = {
   SET_CUSTOMERS(state, customers) {
     state.customers = customers;
   },
- ADD_CUSTOMER(state, newCustomer) {
-  state.customers.push(newCustomer);
-},
+  ADD_CUSTOMER(state, newCustomer) {
+    state.customers.push(newCustomer);
+  },
   UPDATE_CUSTOMER(state, updatedCustomer) {
-    
     // Find the index of the customer to update in the array
-    const index = state.customers.findIndex((customer) => customer.id === updatedCustomer.id);
+    const index = state.customers.findIndex(
+      (customer) => customer.id === updatedCustomer.id
+    );
     if (index !== -1) {
       // Update the customer data in the array
       state.customers.splice(index, 1, updatedCustomer);
     }
-},
-// Mutation in customer.js
-DELETE_CUSTOMER(state, customerId) {
-  const index = state.customers.findIndex((customer) => customer.id === customerId);
-  if (index !== -1) {
-    state.customers.splice(index, 1);
-  }
-}
-
-
-
+  },
+  // Mutation in customer.js
+  DELETE_CUSTOMER(state, customerId) {
+    const index = state.customers.findIndex(
+      (customer) => customer.id === customerId
+    );
+    if (index !== -1) {
+      state.customers.splice(index, 1);
+    }
+  },
 };
-
 
 const actions = {
   async savedCustomers({ commit }) {
     // Commit the mutation to update the state
     const localData = JSON.parse(localStorage.getItem("customers")) || [];
     commit("SET_CUSTOMERS", localData);
-    
   },
-    // async fetchCustomers({ commit }) {
-    //     // console.error("Fetching customers...");
-    //     try {
-    //       const response = await axios.get("http://localhost:3000/customers/");
-    //       if (response.status === 200) {
-    //         commit("SET_CUSTOMERS", response.data);
-    //       } else {
-    //         console.error("Failed to fetch customer data.");
-    //       }
-    //     } catch (error) {
-    //       console.error("Error fetching customer data:", error);
-    //     }
-      
-    //   },
-    async addCustomer({ commit }, newCustomer) {
-      try {
-        // Simultaneously commit the mutation to update the store
-        commit("ADD_CUSTOMER", newCustomer);
-        console.log("updated costumers: ")
-        // Update local storage
-        const savedCustomers = JSON.parse(localStorage.getItem("customers") || "[]");
-        savedCustomers.push(newCustomer);
+  // async fetchCustomers({ commit }) {
+  //     // console.error("Fetching customers...");
+  //     try {
+  //       const response = await axios.get("http://localhost:3000/customers/");
+  //       if (response.status === 200) {
+  //         commit("SET_CUSTOMERS", response.data);
+  //       } else {
+  //         console.error("Failed to fetch customer data.");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching customer data:", error);
+  //     }
+
+  //   },
+  async addCustomer({ commit }, newCustomer) {
+    try {
+      // Simultaneously commit the mutation to update the store
+      commit("ADD_CUSTOMER", newCustomer);
+      console.log("updated costumers: ");
+      // Update local storage
+      const savedCustomers = JSON.parse(
+        localStorage.getItem("customers") || "[]"
+      );
+      savedCustomers.push(newCustomer);
+      localStorage.setItem("customers", JSON.stringify(savedCustomers));
+    } catch (error) {
+      console.error("Error adding customer:", error);
+    }
+  },
+  async updateCustomer({ commit }, updatedCustomer) {
+    try {
+      // Commit a mutation to update the customer in the store
+      commit("UPDATE_CUSTOMER", updatedCustomer);
+
+      // Update local storage
+      const savedCustomers = JSON.parse(
+        localStorage.getItem("customers") || "[]"
+      );
+      const index = savedCustomers.findIndex(
+        (customer) => customer.id === updatedCustomer.id
+      );
+      if (index !== -1) {
+        savedCustomers.splice(index, 1, updatedCustomer);
         localStorage.setItem("customers", JSON.stringify(savedCustomers));
-      } catch (error) {
-        console.error("Error adding customer:", error);
       }
-    },
-    async updateCustomer({ commit }, updatedCustomer) {
-      try {
-        // Commit a mutation to update the customer in the store
-        commit("UPDATE_CUSTOMER", updatedCustomer);
-  
-        // Update local storage
-        const savedCustomers = JSON.parse(localStorage.getItem("customers") || "[]");
-        const index = savedCustomers.findIndex((customer) => customer.id === updatedCustomer.id);
-        if (index !== -1) {
-          savedCustomers.splice(index, 1, updatedCustomer);
-          localStorage.setItem("customers", JSON.stringify(savedCustomers));
-        }
-  
-      
-        return Promise.resolve(); 
-      } catch (error) {
-        console.error("Error updating customer:", error);
-   
-        return Promise.reject(error); 
-      }
-    },
-async deleteCustomer({ commit }, customerId) {
-  try {
-    
-    commit("DELETE_CUSTOMER", customerId);
 
-    // Update local storage
-    const savedCustomers = JSON.parse(localStorage.getItem("customers") || "[]");
-    const updatedCustomers = savedCustomers.filter((customer) => customer.id !== customerId);
-    console.log("updated costumers: "+ updatedCustomers)
-    localStorage.setItem("customers", JSON.stringify(updatedCustomers));
-    
-   
-    return Promise.resolve();
+      return Promise.resolve();
+    } catch (error) {
+      console.error("Error updating customer:", error);
 
-  }catch (error) {
-    console.error("Error deleting customer:", error);
-    
-    return Promise.reject(error); 
-  }
-}
-  };
-   
+      return Promise.reject(error);
+    }
+  },
+  async deleteCustomer({ commit }, customerId) {
+    try {
+      commit("DELETE_CUSTOMER", customerId);
+      console.log("CustomerId:", customerId);
 
+      // Update local storage
+      const savedCustomers = JSON.parse(
+        localStorage.getItem("customers") || "[]"
+      );
+      const updatedCustomers = savedCustomers.filter(
+        (customer) => customer.id !== customerId
+      );
+      console.log("updated costumerssss: " + customerId);
+      localStorage.setItem("customers", JSON.stringify(updatedCustomers));
+
+      return Promise.resolve();
+    } catch (error) {
+      console.error("Error deleting customer:", error);
+
+      return Promise.reject(error);
+    }
+  },
+};
 
 const getters = {
   allCustomers(state) {
@@ -120,8 +123,6 @@ const getters = {
   getCustomerById: (state) => (id) => {
     return state.customers.find((customer) => customer.id === id);
   },
-  
- 
 };
 
-export default { namespaced: true, state, mutations, actions, getters, };
+export default { namespaced: true, state, mutations, actions, getters };
